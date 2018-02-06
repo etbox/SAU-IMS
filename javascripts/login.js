8966dda9679d538cfeaab6e71ba6c306dbc4e90d
etbox <13318972468@126.com>
2018-02-06 20:41:47 +0800

    登录基本完成，就等管理页面切好之后跳转进去

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,74 +1,60 @@
-(function() {
+(function(window) {
   'use strict';
   var $ = window.jQuery;
 
   function showLog() { /*把登陆界面显示出来*/
     var target0 = document.getElementById('login');
-    var target = document.getElementById('back');
-    var target1 = document.getElementById('LoginBoard');
-    var target2 = document.getElementById('x');
     target0.style.display = 'block';
-    target.style.display = 'block';
-    target1.style.display = 'block';
-    target2.style.display = 'block';
   }
 
   function hiddenLog() { /*隐藏界面*/
     var target0 = document.getElementById('login');
-    var target = document.getElementById('back');
-    var target1 = document.getElementById('LoginBoard');
-    var target2 = document.getElementById('x');
     target0.style.display = 'none';
-    target.style.display = 'none';
-    target1.style.display = 'none';
-    target2.style.display = 'none';
   }
 
   function checkUserName(obj) {
     var username = obj;
-    var checkUserNameResult = document.getElementById('logNum');
-    var pw = document.getElementById('psw');
-    var notok = document.getElementById('nameNotNull');
-    var ok = document.getElementById('nameNotNull');
-    var notokimg = document.getElementById('F');
-    var okimg = document.getElementById('T');
+    var notOK = document.getElementById('nameNotNull');
+    var notOKimg = document.getElementById('F').style;
+    var OKimg = document.getElementById('T').style;
 
-    if (username.trim().length == 0) {
-      notok.style.display = 'block';
-      notokimg.style.display = 'block';
-      notokimg.style.left = '68%';
-      notokimg.style.top = '95px';
-      // obj.focus();
+    if (username.trim().length === 0) {
+      notOK.style.display = 'block';
+
+      // console.log(notOK.firstChild.data);
+
+      OKimg.display = 'none';
+      notOKimg.display = 'block';
+      notOKimg.left = '68%';
+      notOKimg.top = '95px';
     } else {
-      notok.style.display = 'none';
-      notokimg.style.display = 'none';
-      okimg.style.display = 'block';
-      okimg.style.left = '68%';
-      okimg.style.top = '95px';
-
+      notOK.style.display = 'none';
+      notOKimg.display = 'none';
+      OKimg.display = 'block';
+      OKimg.left = '68%';
+      OKimg.top = '95px';
     }
   }
 
   function checkPassword(obj) {
     var password = obj;
-    var checkUserNameResult = document.getElementById('psw');
-    var notok = document.getElementById('pswNotNull');
-    var ok = document.getElementById('pswNotNull');
-    var Namenotok = document.getElementById('nameNotNull');
-    var notokimg = document.getElementById('F1');
-    var okimg = document.getElementById('T1');
-    if (password.trim().length == 0) {
-      notok.style.display = 'block';
-      notokimg.style.display = 'block';
-      notokimg.style.left = '68%';
-      notokimg.style.top = '160px';
-      // obj.focus();
+    var notOK = document.getElementById('pswNotNull');
+    var notOKimg = document.getElementById('F1');
+    var OKimg = document.getElementById('T1');
+    if (password.trim().length === 0) {
+      notOK.style.display = 'block';
+
+      OKimg.style.display = 'none';
+      notOKimg.style.display = 'block';
+      notOKimg.style.left = '68%';
+      notOKimg.style.top = '160px';
     } else {
-      notok.style.display = 'none';
-      notokimg.style.display = 'none';
-      okimg.style.display = 'block';
-      okimg.style.left = '68%';
-      okimg.style.top = '160px';
+      notOK.style.display = 'none';
+
+      notOKimg.style.display = 'none';
+      OKimg.style.display = 'block';
+      OKimg.style.left = '68%';
+      OKimg.style.top = '160px';
     }
   }
 
@@ -77,28 +63,50 @@
   function sendLoginData() {
     'use strict';
     var formData = {};
-    var url = 'login/go';
     // var $formElement = $('#LoginBoard');
     var formElement = document.querySelector('#LoginBoard');
     $(formElement).serializeArray().forEach(function(item) {
       formData[item.name] = item.value;
     });
     var jsonText = JSON.stringify(formData);
-    console.log(formData);
+    // console.log(formData);
 
-    $.ajax({
+    var url = 'login/go';
+    // var jsonText = JSON.stringify({
+    //   "userName": "s19961234@126.com",
+    //   "password": "123456",
+    //   "captcha": "cabce"
+    // });
+    sendAjax(url, jsonText)
+      .done(getResponse);
+  }
+
+  function sendAjax(url, json) {
+    'use strict';
+    return $.ajax({
       type: 'post',
+      data: json,
       url: url,
-      data: jsonText,
       headers: {
         'Content-type': 'application/json;charset=UTF-8'
       },
-      dataType: 'json',
-      success: function(data, textStatus) {
-        console.log(`data: ${data}`);
-        console.log(`textStatus: ${textStatus}`);
-      }
+      dataType: 'json'
     });
+  }
+
+  function getResponse(response) {
+    'use strict';
+    var notOK = document.getElementById('pswNotNull');
+    var notOKimg = document.getElementById('F1');
+    if (response.code === 0) {
+      // TODO: 链接未定
+    } else {
+      notOKimg.style.display = 'block';
+      notOKimg.style.left = '68%';
+      notOKimg.style.top = '160px';
+      notOK.style.display = 'block';
+      notOK.firstChild.data = response.msg;
+    }
   }
 
   function addHandler(id, action, func) {
@@ -124,4 +132,4 @@
 
   hiddenLog();
   init();
-}());
+}(window));
